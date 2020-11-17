60ff815eb93593ee77bc2c8d1a80c6582d528cde
Archana-Bhamare <archusonar2@gmail.com>
2020-11-17 19:12:31 +0530

    Added : Check two lines are equal or not

--- src/com/Bridgelabz/LineComparison/LineComparison.java
+++ src/com/Bridgelabz/LineComparison/LineComparison.java
@@ -2,16 +2,30 @@ package com.Bridgelabz.LineComparison;
 
 public class LineComparison {
 	public static void main(String[] args) {
-		double lineLength = 0;
+		double lineLength1;
+		double lineLength2;
 
 		double x1 = (int) (Math.floor(Math.random() * 10));
 		double x2 = (int) (Math.floor(Math.random() * 10));
 		double y1 = (int) (Math.floor(Math.random() * 10));
 		double y2 = (int) (Math.floor(Math.random() * 10));
+		double x3 = (int) (Math.floor(Math.random() * 10));
+		double x4 = (int) (Math.floor(Math.random() * 10));
+		double y3 = (int) (Math.floor(Math.random() * 10));
+		double y4 = (int) (Math.floor(Math.random() * 10));
 
-		lineLength = (Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
+		lineLength1 = (Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
+		lineLength2 = (Math.sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3))));
 
-		System.out.println("Length of a line is : " + lineLength);
+		System.out.println("Length of a Line 1 is : " + lineLength1);
+		System.out.println("Length of a Line 2 is : " + lineLength2);
+		Double line1 = new Double(lineLength1);
+		Double line2 = new Double(lineLength2);
+		boolean equal = line1.equals(line2);
+		if (equal)
+			System.out.println("Lines are Equal");
+		else
+			System.out.println("Lines are Not Equal");
 
 	}
 }
